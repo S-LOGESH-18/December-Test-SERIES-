ecc0245a82be3d4a08a2620b27bafe8d782443bf
LOGESH S <124980896+S-LOGESH-18@users.noreply.github.com>
2023-12-25 18:51:25 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,14 @@
+// About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <p>
+An Internet of Things (IoT) company is an organization that specializes in developing, manufacturing, or providing services related to IoT technologies. IoT refers to the interconnected network of physical devices, vehicles, buildings, and other items embedded with sensors, software, and network connectivity, allowing them to collect and exchange data.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ App.js
@@ -0,0 +1,37 @@
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <hr />
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
